d9c183f7c8aa5721b210ca23ce7bbb67a0c5fb6e
Enzo Volpato <enzo@Mac.lan>
2026-06-18 23:25:35 +0200

    fix: annotation

--- src/main/java/com/example/planeo_back/domain/enums/ExpenseStatus.java
+++ src/main/java/com/example/planeo_back/domain/enums/ExpenseStatus.java
@@ -1,8 +1,5 @@
 package com.example.planeo_back.domain.enums;
 
-import com.example.tscodegen.annotation.TypeScriptExport;
-
-@TypeScriptExport
 public enum ExpenseStatus {
     PENDING,
     PROCESSED,
